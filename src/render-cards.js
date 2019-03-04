be8c79f66495cc6ec759b2e900f30ce47a7961e9
persimonn <agnese.svjatska@gmail.com>
2019-03-04 23:45:46 +0200

    Lint fix

--- src/render-cards.js
+++ src/render-cards.js
@@ -4,11 +4,8 @@ import getCardElement from '../src/get-card-element';
 const allMoviesCardContainer = document.querySelector(`.films-list .films-list__container`);
 
 export default (cardContainer) => {
-
-  let cardAmount;
-  cardContainer === allMoviesCardContainer ? cardAmount = 7 : cardAmount = 2
-  
+  let cardAmount = cardContainer === allMoviesCardContainer ? 7 : 2;
   for (let i = 0; i < cardAmount; i++) {
-      cardContainer.insertAdjacentHTML(`beforeend`, getCardElement(cardData[0].title, cardData[0].rating, cardData[0].year, cardData[0].duration, cardData[0].genre, cardData[0].imgSrc, cardData[0].description, cardData[0].commentAmount));
-    }
+    cardContainer.insertAdjacentHTML(`beforeend`, getCardElement(cardData[0].title, cardData[0].rating, cardData[0].year, cardData[0].duration, cardData[0].genre, cardData[0].imgSrc, cardData[0].description, cardData[0].commentAmount));
+  }
 };
