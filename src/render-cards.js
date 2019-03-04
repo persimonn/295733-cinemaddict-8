fc3b75f9cfb34862ccbdd15bcc3d1a0bed9a8e3f
persimonn <agnese.svjatska@gmail.com>
2019-03-04 23:54:32 +0200

    render-cards changes

--- src/render-cards.js
+++ src/render-cards.js
@@ -4,7 +4,7 @@ import getCardElement from '../src/get-card-element';
 const allMoviesCardContainer = document.querySelector(`.films-list .films-list__container`);
 
 export default (cardContainer) => {
-  let cardAmount = cardContainer === allMoviesCardContainer ? 7 : 2;
+  const cardAmount = cardContainer === allMoviesCardContainer ? 7 : 2;
   for (let i = 0; i < cardAmount; i++) {
     cardContainer.insertAdjacentHTML(`beforeend`, getCardElement(cardData[0].title, cardData[0].rating, cardData[0].year, cardData[0].duration, cardData[0].genre, cardData[0].imgSrc, cardData[0].description, cardData[0].commentAmount));
   }
